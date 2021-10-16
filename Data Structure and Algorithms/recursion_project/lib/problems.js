e9616420f7c82143a065dfc86bd1185f650db1ef
kholood-ea <kholood_ea@hotmail.com>
2021-10-16 19:51:42 +0200

    exponentiation in recursion solved

--- Data Structure and Algorithms/recursion_project/lib/problems.js
+++ Data Structure and Algorithms/recursion_project/lib/problems.js
@@ -75,7 +75,14 @@ function reverseString(str) {
 // pow(2, 5)    // => 32
 // pow(3, 4)    // => 81
 // pow(2, -5)   // => 0.03125
-function pow(base, exponent) {}
+function pow(base, exponent) {
+  if (exponent === 0) {
+    return 1;
+  }
+  if (exponent < 0) {
+    return 1 / pow(base, -exponent);
+  } else return base * pow(base, exponent - 1);
+}
 
 // A 1-dimensional array is also known as a flattened array.
 // Write a method, flatten(data), that accepts a single argument. The
