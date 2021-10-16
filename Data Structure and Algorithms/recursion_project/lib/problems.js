9c8cb79b43218832537318ba4653d5e72a143981
kholood-ea <kholood_ea@hotmail.com>
2021-10-16 21:39:18 +0200

    find file in directory using recursion solved

--- Data Structure and Algorithms/recursion_project/lib/problems.js
+++ Data Structure and Algorithms/recursion_project/lib/problems.js
@@ -131,39 +131,45 @@ function flatten(data) {
 // Example:
 //
 // let desktop = {
-//     '/images': {
-//         'app_academy_logo.svg': null,
-//         '/parks': {
-//             'yosemite.jpeg': null,
-//             'acadia.jpeg': null,
-//             'yellowstone.png': null
-//         },
-//         '/pets': {
-//             'trixie_lou.jpeg': null,
-//             'rolo.jpeg': null,
-//             'opal.jpeg': null,
-//             'diana.jpeg': null,
-//         }
+//   "/images": {
+//     "app_academy_logo.svg": null,
+//     "/parks": {
+//       "yosemite.jpeg": null,
+//       "acadia.jpeg": null,
+//       "yellowstone.png": null,
 //     },
-//     '/music': {
-//         'hey_programmers.mp3': null,
-//         '/genres': {
-//             '/rock': {
-//                 'everlong.flac': null,
-//                 'livin_on_a_prayer.mp3': null
-//             },
-//             '/hip_hop': {
-//                 'express_yourself.wav': null,
-//                 'ny_state_of_mind.mp3': null
-//             }
-//         }
-//     }
+//     "/pets": {
+//       "trixie_lou.jpeg": null,
+//       "rolo.jpeg": null,
+//       "opal.jpeg": null,
+//       "diana.jpeg": null,
+//     },
+//   },
+//   "/music": {
+//     "hey_programmers.mp3": null,
+//     "/genres": {
+//       "/rock": {
+//         "everlong.flac": null,
+//         "livin_on_a_prayer.mp3": null,
+//       },
+//       "/hip_hop": {
+//         "express_yourself.wav": null,
+//         "ny_state_of_mind.mp3": null,
+//       },
+//     },
+//   },
 // };
 //
-// fileFinder(desktop, 'app_academy_logo.svg');     // => true
-// fileFinder(desktop, 'everlong.flac');            // => true
-// fileFinder(desktop, 'sequoia.jpeg');             // => false
-function fileFinder(directories, targetFile) {}
+// fileFinder(desktop, "app_academy_logo.svg"); // => true
+// fileFinder(desktop, "everlong.flac"); // => true
+// fileFinder(desktop, "sequoia.jpeg"); // => false
+function fileFinder(directories, targetFile) {
+  for (let key in directories) {
+    if (key === targetFile || fileFinder(directories[key], targetFile))
+      return true;
+  }
+  return false;
+}
 
 // Write another function, pathFinder(directories, targetFile), that returns the path that contains the targetFile.
 // If the targetFile is not found in the directories, then return null.
